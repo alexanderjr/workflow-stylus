5b8d36ecd6cf6cfbfed5695bc00783bdfbf3b48e
BomfimJr <jbomfimjunior@gmail.com>
2016-05-09 09:26:17 -0300

    First Commit :smile:

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<html>
+<head>
+    <meta charset="UTF-8">   
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>Title</title>
+    <link rel="stylesheet" href="css/style.css">
+	</head>
+<body>
+<script src="js/main.js"></script>
+</body>
+</html>
